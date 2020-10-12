a28ff6dbc3757e239c6fac816d06efed0629cc48
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-10-12 04:37:36 +0300

    done

--- app/src/main/java/kapadokia/nyandoro/bottomsheet/MainActivity.java
+++ app/src/main/java/kapadokia/nyandoro/bottomsheet/MainActivity.java
@@ -3,6 +3,13 @@ package kapadokia.nyandoro.bottomsheet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonShow = findViewById(R.id.buttonShow);
+        buttonShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+                        MainActivity.this, R.style.BottomSheetDialogTheme
+                );
+
+                View bottomSheetView= LayoutInflater.from(getApplicationContext()).inflate(R.layout.layout_bottom_sheet,
+                        (LinearLayout)findViewById(R.id.bottomSheetContainer)
+                );
+
+                bottomSheetView.findViewById(R.id.bottomShare).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Share...", Toast.LENGTH_SHORT).show();
+                        bottomSheetDialog.dismiss();
+                    }
+                });
+
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+            }
+        });
     }
 }
